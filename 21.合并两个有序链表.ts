b3acf1b4e033b871c8f8383009ec67190e49cd0d
huangne01 <1261290560@qq.com>
2021-10-09 15:37:44 +0800

    21.合并两个有序链表

--- 21.合并两个有序链表.ts
+++ 21.合并两个有序链表.ts
@@ -24,25 +24,48 @@
 //     this.next = next === undefined ? null : next
 //   }
 // }
-
+/**
+ * @description: 递归
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ */
 function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  let res = new ListNode(0)
-  let cur = res.next
+  if (l1 === null) return l2
+  if (l2 === null) return l1
+  let node: ListNode
+  if (l1.val < l2.val) {
+    node = new ListNode(l1.val, mergeTwoLists(l1.next, l2))
+  } else {
+    node = new ListNode(l2.val, mergeTwoLists(l1, l2.next))
+  }
+  return node
+}
+/**
+ * @description: 迭代法
+ * 循环l1,l2 判断大小
+ * 重点在于指针的转换
+ */
+function mergeTwoLists2(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let node: ListNode = new ListNode()
+  let prev = node
   while (l1 && l2) {
-    if (l1.val < l2.val) {
-      cur = new ListNode(l1.val)
+    if (l1.val <= l2.val) {
+      prev.next = new ListNode(l1.val)
       l1 = l1.next
     } else {
-      cur = new ListNode(l2.val)
+      prev.next = new ListNode(l2.val)
       l2 = l2.next
     }
-    cur = cur.next
+    prev = prev.next
   }
-  if(l1) cur = l1
-  if(l2) cur = l2
-  return res.next
+  if (l1) prev.next = l1
+  if (l2) prev.next = l2
+  return node.next
 }
 // @lc code=end
