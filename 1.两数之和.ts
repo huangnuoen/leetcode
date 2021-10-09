8c4ae92e30a2c027d377f937f22cf27c75053765
huangne01 <1261290560@qq.com>
2021-10-09 14:43:07 +0800

    两数之和

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -5,11 +5,13 @@
  */
 
 // @lc code=start
+// hash法 O(n)
 function twoSum(nums: number[], target: number): number[] {
   let map = {}
   for (let i = 0; i < nums.length; i++) {
     let diff = target - nums[i]
     if (!(diff in map)) {
+      // 把数组转用map存储，把查找复杂度从O(n)降为O(1)
       map[nums[i]] = i
     } else {
       return [i, map[diff]]
