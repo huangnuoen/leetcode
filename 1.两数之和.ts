74a6f61f143aefac35df819943d10fbbae212f47
huangne01 <1261290560@qq.com>
2021-10-09 14:55:34 +0800

    两数之和

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -5,8 +5,20 @@
  */
 
 // @lc code=start
-// hash法 O(n)
+// 暴力法 O(n^2)
 function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const sum = nums[i] + nums[j]
+      if (sum === target) {
+        return [i, j]
+      }
+    }
+  }
+  return []
+}
+// hash法 O(n)
+function twoSum2(nums: number[], target: number): number[] {
   let map = {}
   for (let i = 0; i < nums.length; i++) {
     let diff = target - nums[i]
